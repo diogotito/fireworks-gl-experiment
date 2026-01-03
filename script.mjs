a460300126f2ca2ce27517766395558d52512960
diogotito <diogotitomarques@gmail.com>
2026-01-03 05:48:33 +0000

    Stretch canvas across page viewport

--- script.mjs
+++ script.mjs
@@ -4,14 +4,14 @@ import Particle from "./Particle.mjs";
 /** @type HTMLCanvasElement */
 const canvas = fireworks
 
-let gl2 = canvas.getContext("webgl2");
-globalThis.gl2 = gl2
-if (!gl2) {
+let gl = canvas.getContext("webgl2");
+globalThis.gl2 = gl
+if (!gl) {
     throw new Error("Failed to get WebGL context!");
 }
 
 
-let particleShader = new G.Shader(gl2, fireworks_vs.text, fireworks_fs.text);
+let particleShader = new G.Shader(gl, fireworks_vs.text, fireworks_fs.text);
 particleShader.use();
 
 let particles = Array.from({length: 10000}, i => Object.assign(new Particle, {
@@ -20,7 +20,7 @@ let particles = Array.from({length: 10000}, i => Object.assign(new Particle, {
     startPos: G.Vec2.polar(Math.random() * Math.PI*2, 5),
     startSize: 20 + 50 * Math.random(),
     startVel: G.Vec2.polar(Math.random() * Math.PI, 6 + 6*Math.random())
-                .add_xy(0, 8 * Math.random()),
+                .add_xy(0, 20 * Math.random()),
     acc: G.vec2(2*Math.random()-1, -9.81 - 10*Math.random()),
     startColor: [
         new G.RGBA(0.95, 0.2, 0.2, 1.0),
@@ -35,14 +35,14 @@ let particles = Array.from({length: 10000}, i => Object.assign(new Particle, {
 
 let manyParticles = new Float32Array(particles.flatMap(p => p.asFloats()));
 
-let particleVbo = gl2.createBuffer();
-gl2.bindBuffer(gl2.ARRAY_BUFFER, particleVbo);
-gl2.bufferData(gl2.ARRAY_BUFFER, manyParticles, gl2.STATIC_DRAW);
+let particleVbo = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, particleVbo);
+gl.bufferData(gl.ARRAY_BUFFER, manyParticles, gl.STATIC_DRAW);
 
 const setupAttrib = (shader, attribName, offset=0, size=1) => {
-    const location = gl2.getAttribLocation(shader.program, attribName)
-    gl2.enableVertexAttribArray(location);
-    gl2.vertexAttribPointer(location, size, gl2.FLOAT, false,
+    const location = gl.getAttribLocation(shader.program, attribName)
+    gl.enableVertexAttribArray(location);
+    gl.vertexAttribPointer(location, size, gl.FLOAT, false,
         (new Particle).asFloats().length * 4, offset
     );
 }
@@ -58,24 +58,28 @@ setupAttrib(particleShader, "endColor",   4 + 4 + 4*2 + 4*2 + 4*2 + 4 + 4 + 4*4,
 
 // the loop
 
-gl2.clearColor(0.2, 0.15, 0.35, 1.0);
-// console.log(gl2.getParameter(gl2.COLOR_CLEAR_VALUE));
-let timeLoc = gl2.getUniformLocation(particleShader.program, "time");
+gl.clearColor(0, 0, 0, 0);
+let timeLoc = gl.getUniformLocation(particleShader.program, "time");
 
-gl2.enable(gl2.BLEND);
-gl2.disable(gl2.DEPTH_TEST);
-//gl2.blendFunc(gl2.SRC_ALPHA, gl2.ONE_MINUS_SRC_ALPHA);
-gl2.blendFuncSeparate(gl2.SRC_ALPHA, gl2.ONE_MINUS_SRC_ALPHA, gl2.ONE, gl2.ONE);
-gl2.blendEquationSeparate(gl2.FUNC_ADD, gl2.MAX);
+gl.enable(gl.BLEND);
+gl.disable(gl.DEPTH_TEST);
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
 function drawParticles(time) {
     // console.log("t = %.3f", time / 1000);
     
-    gl2.uniform1f(timeLoc, time / 1000);
-    gl2.clear(gl2.COLOR_BUFFER_BIT | gl2.DEPTH_BUFFER_BIT);
-    gl2.drawArrays(gl2.POINTS, 0, particles.length);
+    gl.uniform1f(timeLoc, time / 1000);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, particles.length);
 
     requestAnimationFrame(drawParticles);
 }
-
 requestAnimationFrame(drawParticles);
+
+function stretchCanvas() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+}
+stretchCanvas();
+window.addEventListener("resize", stretchCanvas);
